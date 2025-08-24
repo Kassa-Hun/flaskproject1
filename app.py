d8dd0584c61ce30d96772553681e11747407f291
Kassahun T. <informativek@gmail.com>
2025-08-24 06:30:31 +0300

    session included

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash,session
 app=Flask(__name__)
 app.secret_key = 'your_secret_key'
 @app.route('/')
@@ -9,6 +9,7 @@ def  hello(name):
     if name != 'admin':
         flash('You are not authorized to access this page.', 'error')
         return redirect(url_for('index'))
-    return render_template('hello.html', name=name) 
+    session['username']=name
+    return render_template('hello.html', name=session['username']) 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
